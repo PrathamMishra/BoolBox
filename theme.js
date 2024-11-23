37b9eb635c96e4db855b8ad341183606c423172f
Pratham Mishra <mishrapratham30@mail.com>
2024-11-24 02:33:23 +0530

    Added new stlying and removed use of canvas api

---
+++ theme.js
@@ -0,0 +1,23 @@
+const themeContainer = document.querySelector("#theme");
+const themeIcon = document.querySelector("#theme-icon");
+const themeText = document.querySelector("#theme-text");
+const html = document.documentElement;
+
+function setTheme(newTheme) {
+    localStorage.setItem('theme', newTheme);
+    html.setAttribute('data-theme', newTheme);
+    if (newTheme == 'light') {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+        themeText.innerText = 'Day';
+    } else {
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
+        themeText.innerText = 'Night';
+    }
+}
+themeContainer.onclick = function () {
+    const currentTheme = html.dataset.theme;
+    const newTheme = currentTheme == 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+}
